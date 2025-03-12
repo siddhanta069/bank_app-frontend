7598610416aafe89feb38cb5ef684ac83a516a76
siddhanta sahoo <siddhanta027z@gmail.com>
2025-03-12 23:45:59 +0530

    menu section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { logo, close, menu } from '../assets'
 import { navLinks } from "../constants"
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false)
   return (
     <nav className="w-full flex justify-between items-center navbar">
       <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
@@ -11,13 +12,23 @@ const NavBar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] text-white mr-10`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length-1 ? 'mr-0' : 'mr-10'} text-white`}
           >
             <a href={`#${nav.id}`}> {nav.title} </a>
           </li>
 
         ))}
       </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img  
+          src={toggle ? close: menu}
+          alt="menu"
+          className='w-[28px] h-[28px] object-contain ' onClick={ () => setToggle((prev) = !prev)}  
+          />
+
+
+      </div>
     </nav>
   )
 }
